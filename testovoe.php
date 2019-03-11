8c230c257ecb97f2d9af764a8e9505cb2af27ad5
CamillaRafikova <48421801+CamillaRafikova@users.noreply.github.com>
2019-03-11 08:28:53 +0700

    Add files via upload

--- testovoe.php
+++ testovoe.php
@@ -1,30 +1,27 @@
 <?php
 define('ROOT',dirname(_FILE_));
 function search($file,$znach){
-	$handle=fopen($file,"r");
-	while (!feof($handle)) {
-		$string=fgets($handle,4000);
-		mb_convert_encoding($string, 'cp1251');
+	$f=fopen($file,"r");
+	while (!feof($f)) {
+		$string=fgets($f,4000);
 		$explodedstring=explode('\x0A', $string);
 		array_pop($explodedstring);
 		foreach ($explodedstring as $key => $value) {
 			$arr[]=explode('/t', $value);
 		}
-		$nachalo=0;
-		$konec=count($arr)-1;
-		while ($nachalo<=$konec){
-			$seredina=floor(($nachalo + $konec)/2);
-			$strnatcmp=strnatcmp($arr[$seredina][0],$znach);
+		$start=0;
+		$end=count($arr)-1;
+		while ($start<=$end){
+			$middle=floor(($start + $end)/2);
+			$strnatcmp=strnatcmp($arr[$middle][0],$znach);
 			if($strnatcmp>0) {
-				$konec=$seredina-1;
+				$end=$middle-1;
 			}
 			elseif ($strnatcmp<0){
-				$nachalo=$seredina+1;
-				$konec=$seredina-1;
+				$start=$middle+1;
 			}
 			else {
-				return $arr[$seredina][1];
-				$konec=$seredina-1;
+				return $arr[$middle][1];
 			}
 		}
 	}
